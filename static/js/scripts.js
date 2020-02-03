8846e7e53594be85bee0ab87c2649fedf2a39b3a
Jmos-Mbugua <johnmbugua849@gmail.com>
2020-02-03 15:20:34 +0300

    create js folder and file to facilitate overlay

---
+++ static/js/scripts.js
@@ -0,0 +1,18 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+
+
+//   $(document).ready(function(){
+//       $("#images .overlay").mouseenter(function(){
+//           $("#text").show("slow");
+//       })
+//       .mouseleave(function(){
+//           $("#text").hide("slow");
+//       });
+//   }
