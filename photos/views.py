6bda5d932cc3b66bfedf243e7ef4037468ee1174
Jmos-Mbugua <johnmbugua849@gmail.com>
2020-02-04 08:12:23 +0300

    Add the search view function to the views file

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 from .models import Post
+from django.db.models import Q
 # Create your views here.
 
 class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
+
+class SearchResultsView(ListView):
+    model = Post
+    template_name = 'search_results.html'
 
     
