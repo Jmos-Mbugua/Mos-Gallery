e50963957e12db5665082c0ffcb71c7ad79c569a
Jmos-Mbugua <johnmbugua849@gmail.com>
2020-02-04 14:31:27 +0300

    Modify url path for search photos

--- photos/urls.py
+++ photos/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .views import HomePageView, SearchResultsView
+from .views import HomePageView
 
 urlpatterns = [
-    path('search/', SearchResultsView.as_view(), name='search_results'),
-    path('', HomePageView.as_view(), name='home'),
+    path('', HomePageView.as_view(), name='home')
 ]
