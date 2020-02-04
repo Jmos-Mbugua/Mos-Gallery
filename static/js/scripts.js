966bb7163230d130715923a471d16ef78c8282ba
Jmos-Mbugua <johnmbugua849@gmail.com>
2020-02-04 14:33:17 +0300

    Delete the existing js function for overlay hover

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,18 +1,20 @@
-function on() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  }
-
-
-
-//   $(document).ready(function(){
-//       $("#images .overlay").mouseenter(function(){
-//           $("#text").show("slow");
-//       })
-//       .mouseleave(function(){
-//           $("#text").hide("slow");
-//       });
+// function on() {
+//     document.getElementById("overlay").style.display = "block";
 //   }
+  
+//   function off() {
+//     document.getElementById("overlay").style.display = "none";
+//   }
+function myFunction() {
+  alert("Test scripts");
+}
+
+
+  $(document).ready(function(){
+      $("#overlay").mouseenter(function(){
+          $("#text").show("slow");
+      })
+      .mouseleave(function(){
+          $("#text").hide("slow");
+      });
+  })
